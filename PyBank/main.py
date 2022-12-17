d5b21c0d2531bc9d6018b1032da0353a79d30180
lilphonsdale <118135886+lilphonsdale@users.noreply.github.com>
2022-12-17 11:29:56 -0800

    progress on calculating average changes
    but  now the dates and changes are misalsigned

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,116 +1,63 @@
-#imports
-
+# imports
 import os
 import csv
+# find current working directory and set file path
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+pybank_csv = os.path.join("Resources","budget_data.csv")
 
-#set file path
-
-pybank_csv = os.path.join("/Users/oaklandsveryown/Documents/GitHub/python-challenge/PyBank/","Resources","budget_data.csv")
-
-#make lists for the data
-
+# make lists for the data
 dates = []
 results = []
 changes = []
+changes_backup = []
 
-#make a variable for tracking change
+# make a boolean variable for the first row
+is_first_row = True
 
-previous = 0
-
-#make counting variables
-
-#months = 0
-#profit_loss = 0
-
-#read the csv 
-
+# read the csv 
 with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile,delimiter=",")
-
-    #store the header
+    # store the header
     header = next(csvreader)
-
-    #loop through the data and pull the data into lists
-
+    # loop through the data and pull the data into lists
     for row in csvreader:
-
-    #pull data into lists
-
+        # pull data into lists
         dates.append(row[0])
-        results.append(row[1])
-
-        #months += row[0]
-        #profit_loss += row[1]
-
-    #calculate month to month changes
-
+        results.append(int(row[1]))
+        # calculate changes from month to month
         profit_loss = int(row[1])
-        change = profit_loss - previous
-        changes.append(change)
+        if not is_first_row:
+            change = profit_loss - previous
+            changes.append(change)
+        # prepare for next row
+        is_first_row = False
         previous = profit_loss
 
-#look at the list of results and measure the change from month to month
-
-#for i in results[1:]:
-#        change = int(previous) - int(row)
-#        previous = row
-#        changes.append(change)
-
-
-#function to find the total amount of profit/loss
-
-def sum(results):
-    total = 0.0
-    for result in results:
-        total += int(result)
-    return total
-
-#function to find the average changes
-
+# function to find the average changes
 def average(changes):
-    length = len(changes)
-    total = 0.0
-    for change in changes:
-        total += int(change)
+    length = len(changes) 
+    total = sum(changes)
     return total/ length
 
+# return resuilts of analysis in terminal
 print("Financial Analysis")
 print("-----------------------------------")
-
 # The total number of months included in the dataset
-
 print(f'Total Months: {len(dates)}')
-
 # The net total amount of "Profit/Losses" over the entire period
-
 print(f'Total :{sum(results)}')
-
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-
-print(f'Average Change : {average(changes)}')
-
-
-#for this we should subtract row+1 from row as we loop through the sheet, appending the results to a new dict. 
-# then we can average the values in that dict 
-
+print(f'Average Change :$ {(average(changes))}')
 #here we should zip the dates to the changes and then search that tuple
-
 zipped_data = zip(dates,changes)
-
 # The greatest increase in profits (date and amount) over the entire period
-
 print(f'Greatest Increase in Profits:')
-
 for row in zipped_data:
     if row[1] == max(changes):
         print(row)
-
 # The greatest decrease in profits (date and amount) over the entire period
-
 zipped_data = zip(dates,changes)
-
 print(f'Greatest Decrease in Profits:')
-
 for row in zipped_data:
     if row[1] == min(changes):
         print(row)
---
+++ PyPoll/main.py
@@ -0,0 +1,37 @@
+# imports
+import os
+import csv
+# find current working directory and set file path
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+pypoll_csv = os.path.join("Resources","election_data.csv")
+
+#make lists to store the data
+voter_id = []
+county = []
+candidate = []
+
+#read the csv 
+with open(pypoll_csv) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    #store the header
+    header = next(csvreader)
+    #loop through the data and pull the data into lists
+    for row in csvreader:
+    #pull data into lists
+        voter_id.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+
+#The total number of votes cast
+total_votes = len(voter_id)
+print(f'Total Votes : {total_votes}' )
+
+#A complete list of candidates who received votes
+candidates = list(set(candidate))
+print(candidates)
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote
